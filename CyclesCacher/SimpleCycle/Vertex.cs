1620eadd3a9bea4cf14562b86c94b6f19951b966
sharno <sharnoby3@gmail.com>
2015-10-19 16:28:46 +0300

    elementary cycles enumeration

---
+++ CyclesCacher/SimpleCycle/Vertex.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Metabol;
+
+namespace CyclesCacher.SimpleCycle
+{
+    class Vertex
+    {
+        public Guid Id;
+        public HashSet<Guid> Next;
+    }
+}
